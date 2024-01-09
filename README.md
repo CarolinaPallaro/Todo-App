011babf2866e65600896214e850702e1bddf9bf7
Carolina Pallaro <charopallaro@gmail.com>
2024-01-09 15:13:03 -0300

    creando entorno

---
+++ README.md
@@ -0,0 +1,5 @@
+https://www.youtube.com/watch?v=8GFmoynezyA 
+
+https://www.youtube.com/watch?v=QdTHUv79EZc&t=249s
+
+https://tailwindui.com/components
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <div>   
+      <div className='container'>
+
+        <h1 className=''>Todo App</h1>
+       
+
+      </div>
+    
+    </div>
+ 
+
+  );
+}
+
+export default App
